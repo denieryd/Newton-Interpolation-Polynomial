25e60cc4507ebac8e1716d38435c56fdaa5a7fc0
Denis Blinkov <d.blinkovv@yandex.ru>
2019-11-16 09:43:58 +0300

    build program

--- main.py
+++ main.py
@@ -1,54 +1,50 @@
-# creating first newton polynomial
-from math import factorial
-
-x_dots = [-0.5, 0, 0.5, 1]
-y_dots = [1, 2, -1, 1]
-
-coef_y = []
-for j in range(len(x_dots)):
-    if j == 0:
-        coef_y.append(y_dots)
-    else:
-        tmp_list = []
-        for v in range(len(y_dots) - j):
-            tmp_list.append(1)
-        coef_y.append(tmp_list)
-
-print(coef_y)
-
-step = 0.5
-
-for current_list_pointer in range(1, len(coef_y)):
-    for i in range(len(coef_y[current_list_pointer - 1]) - 1):
-        current_coeffs = coef_y[current_list_pointer - 1]
-        new_delta_y = current_coeffs[i + 1] - current_coeffs[i]
-        coef_y[current_list_pointer][i] = new_delta_y
-        print(new_delta_y)
-
-print(coef_y)
-
-for i in range(len(coef_y)):
-    print(coef_y[i][0])
+from newton_poly_with_uniform_nodes import calculate_newton_polynomial_uniform_nodes
+from newton_poly_with_optimal_nodes import calculate_newton_polynomial_optimal_nodes
 
 
-def calculate_polynomial():
-    inputed_x = float(input('Введите x: '))
-    polynomial_sum = 0
+def get_user_choice():
+    while True:
+        try:
+            user_choice = int(input('1.Посчитать сумму через оптимальные узлы\n'
+                                    '2.Посчитать сумму через равномерные узлы\n'
+                                    '3.Завершить программу\n'
+                                    '>>>: '))
+            if user_choice not in [1, 2, 3]:
+                raise ValueError
+            break
+        except ValueError:
+            print('Ошибка. Повторите ввод')
 
-    for i in range(0, len(coef_y)):
-        if i == 0:
-            polynomial_sum += coef_y[i][0]  # [0][0]
-        else:
-            first_part_term = coef_y[i][0] / (factorial(i) * (step ** i))
-            second_part_term = 1  # product should be here
-            for j in range(i):
-                second_part_term *= (inputed_x - x_dots[j])
-            result_term = first_part_term * second_part_term
-            polynomial_sum += result_term
-
-    print(polynomial_sum)
-    print(12* (inputed_x**3) - 8 * (inputed_x ** 2) - 5* inputed_x + 2)
+    return user_choice
 
 
+def get_coords():
+    x_dots = list(map(float, input('Введите x через пробел: ').split()))
+    y_dots = list(map(float, input('Введите y через пробел: ').split()))
+    inputed_x = float(input('Введите икс точки, в которой требуется посчитать значение полинома: '))
 
-calculate_polynomial()
+    return x_dots, y_dots, inputed_x
+
+
+def main():
+    while True:
+        user_choice = get_user_choice()
+
+        if user_choice != 3:
+            x_dots, y_dots, inputed_x = get_coords()
+
+            if user_choice == 1:
+                polynomial_sum = calculate_newton_polynomial_optimal_nodes(x_dots=x_dots, y_dots=y_dots,
+                                                                           inputed_x=inputed_x)
+                print(f'Сумма полиному в точке x = {inputed_x} есть y={polynomial_sum}\n' + '='*40)
+
+            elif user_choice == 2:
+                polynomial_sum = calculate_newton_polynomial_uniform_nodes(x_dots=x_dots, y_dots=y_dots,
+                                                                           inputed_x=inputed_x)
+                print(f'Сумма полинома в точке х={inputed_x} есть у={polynomial_sum}\n' + '='*40)
+        if user_choice == 3:
+            print('Программа завершена.')
+            break
+
+
+main()
