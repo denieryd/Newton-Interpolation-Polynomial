e6bade99ba44363c80b0aaedb03fd4bfaf24226e
Denis Blinkov <d.blinkovv@yandex.ru>
2019-11-24 14:50:26 +0300

    stable version

--- main_w_gui.py
+++ main_w_gui.py
@@ -14,131 +14,105 @@ from tools import get_x_dots_optimal, get_y_dots_optimal, get_additional_dots, c
 
 matplotlib.use('TkAgg')
 
+window_width = 1500
+window_height = 900
+root = Tk()
+root.title("Вычисление первого полинома Ньютона")
+root.geometry(f'{window_width}x{window_height}')
 
-class MyApp:
-    def __init__(self, width=1500, height=900):
-        self.window_width = width
-        self.window_height = height
-        self.root = Tk()
-        self.root.title("Вычисление первого полинома Ньютона")
-        self.root.geometry(f'{self.window_width}x{self.window_height}')
+tab_control = ttk.Notebook(root)
 
-        self.tab_control = ttk.Notebook(self.root)
+reading_console_tab = ttk.Frame(tab_control)
+tab_control.add(reading_console_tab, text='Считывание с консоли')
+tab_control.pack(expand=1, fill='both')
 
-        self.fig = Figure(figsize=(3, 3), dpi=120)
+example_tab = ttk.Frame(tab_control)
+tab_control.add(example_tab, text='Эталонный пример')
+tab_control.pack(expand=1, fill='both')
 
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)  # A tk.DrawingArea.
-        self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+reading_file_tab = ttk.Frame(tab_control)
+tab_control.add(reading_file_tab, text='Считывание с файла')
+tab_control.pack(expand=1, fill='both')
 
-    def setup_tabs(self):
-        self.reading_console_tab = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.reading_console_tab, text='Считывание с консоли')
-        self.tab_control.pack(expand=1, fill='both')
+header_label = Label(example_tab, text="Сравнение оптимальных и равномерных узлов"
+                                       "\nдля функции 12x^3-8x^2-5x+2",
+                     font=("Arial Bold", 13, 'bold'))
+header_label.grid(column=0, row=0)
+label_input_iteration = Label(example_tab, text="Выберите n", font=("Arial Bold", 11))
+label_input_iteration.grid(column=0, row=1)
 
-        self.example_tab = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.example_tab, text='Эталонный пример')
-        self.tab_control.pack(expand=1, fill='both')
+label_borders = Label(example_tab, text="Введите a и b для оптимальных узлов", font=("Arial Bold", 11))
 
-        self.reading_file_tab = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.reading_file_tab, text='Считывание с файла')
-        self.tab_control.pack(expand=1, fill='both')
+label_borders.grid(column=0, row=2)
 
-    def setup_example_tab_labels(self):
-        self.header_label = Label(self.example_tab, text="Сравнение оптимальных и равномерных узлов"
-                                                         "\nдля функции 12x^3-8x^2-5x+2",
-                                  font=("Arial Bold", 13, 'bold'))
-        self.header_label.grid(column=0, row=0)
-        self.label_input_iteration = Label(self.example_tab, text="Выберите n", font=("Arial Bold", 11))
-        self.label_input_iteration.grid(column=0, row=1)
+label_input_set_x_ex = Label(example_tab, text="Введите множество x через пробел", font=("Arial Bold", 11))
+label_input_set_x_ex.grid(column=0, row=3)
 
-        self.label_borders = Label(self.example_tab, text="Введите a и b для оптимальных узлов",
-                                   font=("Arial Bold", 11))
+label_input_set_y_ex = Label(example_tab, text="Введите множество y через пробел", font=("Arial Bold", 11))
+label_input_set_y_ex.grid(column=0, row=4)
 
-        self.label_borders.grid(column=0, row=2)
+label_input_set_y_ex = Label(example_tab, text="Введите точку х, для вычисление P(x)",
+                             font=("Arial Bold", 11))
+label_input_set_y_ex.grid(column=0, row=5)
 
-        self.label_input_set_x_ex = Label(self.example_tab, text="Введите множество x через пробел",
-                                          font=("Arial Bold", 11))
-        self.label_input_set_x_ex.grid(column=0, row=3)
+count_n = StringVar(root)
+count_n.set('4')
+count_n_switching_menu = OptionMenu(example_tab, count_n, "4", "8")
 
-        self.label_input_set_y_ex = Label(self.example_tab, text="Введите множество y через пробел",
-                                          font=("Arial Bold", 11))
-        self.label_input_set_y_ex.grid(column=0, row=4)
+count_n_switching_menu.grid(column=1, row=1)
 
-        self.label_input_set_y_ex = Label(self.example_tab, text="Введите точку х, для вычисление P(x)",
-                                          font=("Arial Bold", 11))
-        self.label_input_set_y_ex.grid(column=0, row=5)
+label_borders = Label(example_tab, text="Введите a и b для оптимальных узлов", font=("Arial Bold", 11))
+input_borders = Entry(example_tab, width=20)
+label_borders.grid(column=0, row=2)
+input_borders.grid(column=1, row=2, pady=10)
 
-    def setup_example_tab(self):
-        self.count_n = StringVar(self.root)
-        self.count_n.set('4')
-        self.count_n_switching_menu = OptionMenu(self.example_tab, self.count_n, "4", "8")
+label_input_set_x_ex = Label(example_tab, text="Введите множество x через пробел", font=("Arial Bold", 11))
+input_set_x_ex = Entry(example_tab, width=20)
+label_input_set_x_ex.grid(column=0, row=3)
+input_set_x_ex.grid(column=1, row=3, pady=10)
 
-        self.count_n_switching_menu.grid(column=1, row=1)
+label_input_set_y_ex = Label(example_tab, text="Введите множество y через пробел", font=("Arial Bold", 11))
+input_set_y_ex = Entry(example_tab, width=20)
+input_set_y_ex.grid(column=1, row=4, pady=10)
+label_input_set_y_ex.grid(column=0, row=4)
 
-        self.input_borders = Entry(self.example_tab, width=20)
-        self.input_borders.grid(column=1, row=2, pady=10)
-        self.input_set_x_ex = Entry(self.example_tab, width=20)
-        self.input_set_x_ex.grid(column=1, row=3, pady=10)
+label_input_set_y_ex = Label(example_tab, text="Введите точку х, для вычисление P(x)", font=("Arial Bold", 11))
+input_point_x_ex = Entry(example_tab, width=20)
+label_input_set_y_ex.grid(column=0, row=5)
+input_point_x_ex.grid(column=1, row=5, pady=10)
 
-        self.input_set_y_ex = Entry(self.example_tab, width=20)
-        self.input_set_y_ex.grid(column=1, row=4, pady=10)
+fig = Figure(figsize=(3, 3), dpi=120)
 
-        self.input_point_x_ex = Entry(self.example_tab, width=20)
-        self.input_point_x_ex.grid(column=1, row=5, pady=10)
-
-        self.btn_calculate_polynomial_ex_tab = Button(self.example_tab, text="Найти значение полинома",
-                                                      command=calculate_polynomial_from_example_tab,
-                                                      width=25)
-
-        self.btn_calculate_polynomial_ex_tab.grid(column=2, row=5, padx=15, pady=5)
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 
 
 # //////////////
 
+def create_draw_from_gui(x_dots, y_dots, nodes_type):
+    if nodes_type == 0:
+        lbl = 'Равномерные узлы'
+    else:
+        lbl = 'Оптимальные узлы'
 
-class GUIWorker:
-    def __init__(self):
-        pass
+    fig.clear()
+    ax = fig.add_subplot(111)
+    ax.plot(x_dots, y_dots, label=lbl)
+    ax.legend()
+    canvas.draw_idle()
 
-    def create_draw_from_gui(self, x_dots, y_dots, nodes_type, fig, canvas):
-        if nodes_type == 0:
-            lbl = 'Равномерные узлы'
-        else:
-            lbl = 'Оптимальные узлы'
 
-        fig.clear()
-        ax = fig.add_subplot(111)
-        ax.plot(x_dots, y_dots, label=lbl)
-        ax.legend()
-        canvas.draw_idle()
+def create_draw_file_tab(x_dots, y_dots, nodes_type):
+    if nodes_type == 0:
+        lbl = 'Равномерные узлы'
+    else:
+        lbl = 'Оптимальные узлы'
 
-    def create_draw_file_tab(self, x_dots, y_dots, nodes_type, fig, canvas):
-        if nodes_type == 0:
-            lbl = 'Равномерные узлы'
-        else:
-            lbl = 'Оптимальные узлы'
-
-        fig.clear()
-        ax = fig.add_subplot(111)
-        ax.plot(x_dots, y_dots, label=lbl)
-        ax.legend()
-        canvas.draw_idle()
-
-    def create_draw_ex_tab(self, x_dots_polynomial=None, y_dots_uniform=None, y_dots_optimal=None, fig, canvas):
-        pattern_func_x_dots = np.linspace(-10, 13, 200)
-        pattern_func_y_dots = []
-        for x_dot in pattern_func_x_dots:
-            pattern_func_y_dots.append(pattern_func(x_dot))
-
-        fig.clear()
-        ax = fig.add_subplot(111)
-        if y_dots_uniform is not None:
-            ax.plot(x_dots_polynomial, y_dots_uniform, label='равномерные узлы')
-        if y_dots_optimal is not None:
-            ax.plot(x_dots_polynomial, y_dots_optimal, label='оптимальные узлы')
-        ax.plot(pattern_func_x_dots, pattern_func_y_dots, label='12x^3-8x^2-5x+2')
-        ax.legend()
-        canvas.draw_idle()
+    fig.clear()
+    ax = fig.add_subplot(111)
+    ax.plot(x_dots, y_dots, label=lbl)
+    ax.legend()
+    canvas.draw_idle()
 
 
 def calculate_polynomial_from_gui_interface():
@@ -214,6 +188,26 @@ def pattern_func(x):
     return 12 * (x ** 3) - 8 * (x ** 2) - 5 * x + 2
 
 
+def create_draw_ex_tab(x_dots_polynomial=None, y_dots_uniform=None, y_dots_optimal=None):
+    pattern_func_x_dots = np.linspace(-10, 13, 200)
+    pattern_func_y_dots = []
+    for x_dot in pattern_func_x_dots:
+        pattern_func_y_dots.append(pattern_func(x_dot))
+
+    fig.clear()
+    ax = fig.add_subplot(111)
+    if y_dots_uniform is not None:
+        ax.plot(x_dots_polynomial, y_dots_uniform, label='равномерные узлы')
+    if y_dots_optimal is not None:
+        ax.plot(x_dots_polynomial, y_dots_optimal, label='оптимальные узлы')
+    ax.plot(pattern_func_x_dots, pattern_func_y_dots, label='12x^3-8x^2-5x+2')
+    ax.legend()
+    canvas.draw_idle()
+
+
+create_draw_ex_tab()
+
+
 def calculate_polynomial_from_example_tab():
     """
     Reading data from inputs in the program
@@ -248,6 +242,12 @@ def calculate_polynomial_from_example_tab():
     f'Равномерные узлы P({inputed_x}) = {result_with_uniform}\n'
     f'Значение самой функции: {pattern_func(inputed_x)}')
 
+
+btn_calculate_polynomial_ex_tab = Button(example_tab, text="Найти значение полинома",
+                                         command=calculate_polynomial_from_example_tab,
+                                         width=25)
+
+btn_calculate_polynomial_ex_tab.grid(column=2, row=5, padx=15, pady=5)
 
 # ///////
 
